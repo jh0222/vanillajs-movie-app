dcbcb382b9a029d5796fafc44423f2bbd93f2103
jh0222 <gihyuny0222@gmail.com>
2024-04-11 15:27:56 +0900

    Create project

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+import { Store } from "../core/heropy";
+ 
+export default new Store({
+  photo: 'https://i.namu.wiki/i/cS4lR_Kmb2H-LcVV0ge9eaLsenhaeSnoZ-NhIkiv4xn_GvCYStjYniOKQVAlk6WJaRyXNIkZ2_peC__BnibDFg.webp',
+  name: 'JH / SeoJiHyeon',
+  email: 'gihyuny0222@gmail.com',
+  blog: 'https://velog.io/@jstory0222/posts',
+  github: 'https://github.com/jh0222',
+  repository: 'https://github.com/jh0222/vanillajs-movie-app'
+})
